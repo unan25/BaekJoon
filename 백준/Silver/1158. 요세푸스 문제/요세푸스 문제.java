3a738676db87ca2cef34ec7703aa23d61d901110
Unan <137999702+unan25@users.noreply.github.com>
2024-01-20 14:36:54 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 588 ms, Memory: 296004 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 1158번 요세푸스 문제
+
+// 14:20 ~ 14:35
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < K - 1; i++) {
+                queue.add(queue.poll());
+            }
+            sb.append(queue.poll() + ", ");
+        }
+        sb.replace(sb.length() - 2, sb.length(), ">");
+        System.out.println(sb);
+    }
+}
