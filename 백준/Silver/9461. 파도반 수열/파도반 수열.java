79c4f709ec19438d739523a7040b1941dc6da1d2
Unan <137999702+unan25@users.noreply.github.com>
2024-02-12 16:14:37 +0900

    [Silver III] Title: 파도반 수열, Time: 132 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[101];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 2;
+        dp[4] = 2;
+        dp[5] = 3;
+        dp[6] = 4;
+        dp[7] = 5;
+        dp[8] = 7;
+        dp[9] = 9;
+
+        for (int i = 10; i < 101; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            System.out.println(dp[n - 1]);
+        }
+
+    } // main
+}
