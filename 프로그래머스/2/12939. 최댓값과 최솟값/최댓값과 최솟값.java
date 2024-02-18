9888e955d4a70cc07f105820595b2e1c76a6d475
Unan <137999702+unan25@users.noreply.github.com>
2024-02-18 21:52:34 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.68 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        StringTokenizer st = new StringTokenizer(s, " ");
+        List<Integer> list = new ArrayList<>();
+        while(st.hasMoreTokens()) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        
+        Collections.sort(list);
+        
+        answer = list.get(0) + " " + list.get(list.size() - 1);
+        
+        return answer;
+    }
+}
