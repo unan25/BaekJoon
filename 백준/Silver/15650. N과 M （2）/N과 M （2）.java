caad24cf5b25ee3285fb6a88f20260a0bd7b9bde
Unan <137999702+unan25@users.noreply.github.com>
2024-03-09 17:26:03 +0900

    [Silver III] Title: N과 M (2), Time: 156 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n + 1];
+
+        dfs(1, 0);
+
+        br.close();
+    }
+
+    static void dfs(int now, int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = now; i <= n; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
+}
